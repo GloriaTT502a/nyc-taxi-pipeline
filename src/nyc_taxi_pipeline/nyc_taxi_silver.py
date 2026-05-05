a709813248c90fa7590c7b66b18bf7db07e3b1aa
gloriatt502@gmail.com <gloriatt502@gmail.com>
2026-05-05 19:17:49 +0000

    Update

--- src/nyc_taxi_pipeline/nyc_taxi_silver.py
+++ src/nyc_taxi_pipeline/nyc_taxi_silver.py
@@ -28,7 +28,7 @@ class NYC_Taxi_Silver_Loader:
         checkpoint_schema: str = None,
         ):
         self.spark = spark
-        self.target_table = target_table or PipelineConfig.get_table_path("target_silver"， PipelineConfig.DATABASE)
+        self.target_table = target_table or PipelineConfig.get_table_path("target_silver", PipelineConfig.DATABASE)
         self.audit_table = audit_table or PipelineConfig.get_table_path("pipeline_metrics", PipelineConfig.DATABASE)
         
         self.quarantine_table = f"{self.target_table}_quarantine"
